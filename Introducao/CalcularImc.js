606af1208438593b9903ca2a0b1ad088f06c2e70
rmkaneko <roberto_kaneko@hotmail.com>
2022-01-09 19:51:49 -0300

    Implemented CalcularImc.js

---
+++ Introducao/CalcularImc.js
@@ -0,0 +1,30 @@
+function calcularImc(peso, altura){
+    if((peso !== undefined && altura !== undefined) && typeof peso === "number" && typeof altura === "number" ){
+        return (peso/(altura**2))
+    }
+    throw Error("Need to parameters: weight and heigth")
+}
+console.log(calcularImc(63, 1.65))
+
+function classificaImc(imc){
+    if(typeof imc !== "number" && imc === undefined){
+        throw Error("Need imc or number")
+    }
+    if(imc <= 16.9){
+        return "Muito abaixo do peso"
+    }else if(imc <= 18.4){
+        return "Abaixo do peso"
+    }else if(imc <= 24.9){
+        return "Peso normal"
+    }else if( imc <= 29.9){
+        return "Acima do peso"
+    }else if(imc <= 34.9){
+        return "Obesidade Grau I"
+    }else if(imc <= 40){
+        return "Obesidade grau II"
+    }else {
+        return "Obesidade grau II"
+    }
+}
+
+console.log(classificaImc(calcularImc(62,1.65)))
